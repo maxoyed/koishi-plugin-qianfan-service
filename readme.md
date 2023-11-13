ea7715a6d6685f36cf8fc3dd2c611474659bfbc1
maxoyed <maxoyed@gmail.com>
2023-11-13 20:57:20 +0800

    first commit

--- readme.md
+++ readme.md
@@ -3,3 +3,54 @@
 [![npm](https://img.shields.io/npm/v/koishi-plugin-qianfan-service?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-qianfan-service)
 
 百度智能云千帆大模型平台基础服务
+
+## 配置项
+
+- `API_KEY`: 千帆大模型平台应用 API Key
+- `SECRET_KEY`: 千帆大模型平台应用 Secret Key
+
+## 支持接口
+
+接口请求参数、返回值的数据结构与官网基本一致，具体参考 [千帆大模型平台 - API 列表](https://cloud.baidu.com/doc/WENXINWORKSHOP/s/Nlks5zkzu)
+
+### ctx.qianfan.chat(body: ChatBody, model: ChatModel)
+
+发起对话请求
+
+- `body`: 请求参数
+- `model`: 使用的模型，默认 Ernie-Bot
+- 返回值: ChatResp
+
+### ctx.qianfan.imagine(body: Text2ImageBody)
+
+发起文生图请求
+
+- `body`: 请求参数
+- 返回值: Text2ImageResp
+
+## 调用示例
+
+```typescript
+// 声明服务依赖
+import {} from "koishi-plugin-qianfan-service";
+export const inject = ["qianfan"];
+
+// 对话
+const body = {
+  messages: [
+    {
+      role: "user",
+      content: "Hello World",
+    },
+  ],
+};
+const resp = await ctx.qianfan.chat(body);
+logger.debug(resp.result); // 处理结果
+
+// 绘画
+const body = {
+  prompt: "cute,white,cat",
+};
+const resp = await ctx.qianfan.imagine(body);
+logger.debug(resp.data[0].b64_image); // 处理结果
+```
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,63 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, Service } from "koishi";
+import { Qianfan } from "qianfan";
+import { ChatBody, ChatModel, Text2ImageBody } from "qianfan/dist/interface";
 
-export const name = 'qianfan-service'
+export const name = "qianfan-service";
 
-export interface Config {}
+export interface Config {
+  API_KEY: string;
+  SECRET_KEY: string;
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  API_KEY: Schema.string().required(),
+  SECRET_KEY: Schema.string().required().role("secret"),
+});
 
-export function apply(ctx: Context) {
-  // write your plugin here
+declare module "koishi" {
+  interface Context {
+    qianfan: QianfanService;
+  }
+}
+
+export class QianfanService extends Service {
+  private client: Qianfan;
+  constructor(public ctx: Context, public config: Config) {
+    super(ctx, "qianfan", true);
+    this.client = new Qianfan(config.API_KEY, config.SECRET_KEY);
+  }
+
+  /**
+   * 发起对话请求
+   * @param body 请求体
+   * @param model 使用的模型
+   */
+  async chat<T extends ChatModel>(
+    body: ChatBody<T>,
+    model: T = "ERNIE-Bot-4" as T
+  ) {
+    const resp = await this.client.chat(body, model);
+    this.logger.debug({
+      body,
+      resp,
+    });
+    return resp;
+  }
+
+  /**
+   * 发起文生图请求
+   * @param body 请求体
+   */
+  async imagine(body: Text2ImageBody) {
+    const resp = await this.client.text2image(body);
+    this.logger.debug({
+      body,
+      resp,
+    });
+    return resp;
+  }
+}
+
+export function apply(ctx: Context, config: Config) {
+  ctx.plugin(QianfanService, config);
 }
